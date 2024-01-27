a6ac5a01338fa17ad4df2d7b97f4e11e97831a3d
ttemirlan1 <t.tashbaltaev@gmail.com>
2024-01-27 18:43:14 +0600

    main tasks done

--- module1/src/Search.tsx
+++ module1/src/Search.tsx
@@ -1,40 +1,56 @@
-import { Component } from 'react'
+import { Component, ChangeEvent } from 'react'
 import './Search.css'
 import axios from 'axios'
+
 
 class Search extends Component {
   constructor(props){
     super(props);
     this.state = {
-      apiResult: null
+      apiResult: null,
+      inputValue: ''
     };
   }
-  checkApi = () => {
-    axios.get('https://swapi.dev/api/')
-      .then((result)=> {
-        this.setState({apiResult: result.data})
-        console.log(result.data)
+  handleSearch = (event) => {
+    event.preventDefault()
+    axios.get(`https://swapi.dev/api/${this.state.inputValue}`)
+    .then((result)=> {
+      this.setState({apiResult: result.data})
+        console.log(apiResult)
+        console.log(inputValue)
       })
       .catch((error)=>{
         console.log(error)
+        console.log('asdasd')
       })
   }
-  componentDidMount() {
-    this.checkApi();
+  handleInputChange = (event) => {
+    event.preventDefault()
+    this.setState({
+      inputValue: event.target.value
+    });
   }
+  
   render() {
     return (
       <>
+      <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
         <h1>Search from Star Wars</h1>
-        <input
-          type="text"
-          placeholder="films /poeple /planets /species /starships /vehicles"
-        />
-        <p>{JSON.stringify(this.state.apiResult, null, 2)}</p>
-        <button onClick={this.checkApi}>Search</button>
+        <div style={{ flex: '0 0 auto', backgroundColor: 'lightblue', padding: '10px' }}>
+          <form onSubmit={this.handleSearch}>
+            <input
+              type="text"
+              placeholder="films /people /planets /species /starships /vehicles"
+              value={this.state.inputValue}
+              onChange={this.handleInputChange}
+            />
+            <button type='submit'>Search</button>
+          </form>
+        </div>
       
-        <div className="card">
-          
+        <div style={{ flex: '1', backgroundColor: 'lightgray', padding: '20px' }}>
+        <p>{JSON.stringify(this.state.apiResult, null, 2)}</p>
+        </div>
         </div>
       </>
     )
